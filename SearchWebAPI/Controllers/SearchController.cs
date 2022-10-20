7155826f85b11840f91909101efe09e60bc4d3a4
AliSK <aebrahimi6739@gmail.com>
2022-10-20 19:01:25 +0330

    add project files

---
+++ SearchWebAPI/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentProject;
+
+// https://www.nuget.org/packages/StarAcademy.CodeStar.alisk.SearchEngine
+
+namespace SearchWebAPI.Controllers
+
+{
+    [ApiController]
+    public class SearchController : Controller
+    {
+        [HttpGet("[action]")]
+        public IActionResult Search([FromQuery] string query)
+        {
+            var result = SearchEngine.Instance.Search(query);
+
+            if(result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+    }
+}
